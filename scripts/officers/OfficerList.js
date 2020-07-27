2ecb36fce043d66cf9b19c3b26e2b975381f48e5
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-27 14:29:51 -0500

    changed officerlist.js to avoid coupling with other modules

--- scripts/officers/OfficerList.js
+++ scripts/officers/OfficerList.js
@@ -1,19 +1,26 @@
-import { useOfficers, getOfficers } from "./OfficerProvider.js"
-import { OfficerHTMLConverter } from "./OfficerHTMLConverter.js"
+import {useOfficers, getOfficers} from "./OfficerProvider.js"
+import {OfficerHTMLConverter} from "./OfficerHTMLConverter.js"
 
 const contentTarget = document.querySelector(".officersContainer")
 
+const render = (arrayOfOfficers) => {
+  let officerHTML = ""
+
+  arrayOfOfficers.forEach((officer) => {
+    officerHTML += OfficerHTMLConverter(officer)
+  })
+
+  contentTarget.innerHTML = `
+        <h2>Glassdale Police Officers</h2>
+        <article class="criminalList">
+            ${officerHTML}
+        </article>
+    `
+}
+
 export const OfficerList = () => {
   getOfficers().then(() => {
-    const officerArray = useOfficers()
-    let officerHTMLRepresentations = ""
-    officerArray.forEach((officer) => {
-      officerHTMLRepresentations += OfficerHTMLConverter(officer)
-    })
-
-    contentTarget.innerHTML += `<h2>Glassdale Police Officers</h2> 
-    <article class=""officers__list>
-      ${officerHTMLRepresentations}
-    </article>`
+    const officers = useOfficers()
+    render(officers)
   })
 }
