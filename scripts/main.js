46baae3cd1a85a11f8e419bc331a516ce3433fbd
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-29 14:27:13 -0500

    working on glassdale 7. fields show up, no functionality, json is not working correctly

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,13 @@ import {OfficerList} from "./officers/OfficerList.js"
 import {CriminalList} from "./criminals/CriminalsList.js"
 import {ConvictionSelect} from "./convictions/ConvictionsList.js"
 import {OfficerSelect} from "./officers/OfficerSelect.js"
+import {NoteForm} from "./notes/NoteForm.js"
+import {ShowNoteButton} from "./notes/ShowNoteButton.js"
+import "./notes/NoteList.js"
 
 OfficerList()
 CriminalList()
 ConvictionSelect()
 OfficerSelect()
+NoteForm()
+ShowNoteButton()
--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,10 +1,38 @@
+import {saveNote} from "./NoteProvider.js"
+
 const contentTarget = document.querySelector(".noteFormContainer")
+const eventHub = document.querySelector(".container")
+
+// Handle browser-generated click event in component
+eventHub.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.id === "saveNote") {
+    // Make a new object representation of a note
+
+    const noteTitle = document.querySelector("#note--title")
+    const noteAuthor = document.querySelector("#note--author")
+    const noteContent = document.querySelector("#note--content")
+
+    const newNote = {
+      // Key/value pairs here
+      title: noteTitle.value,
+      author: noteAuthor.value,
+      content: noteContent.value,
+      timestamp: new Date().toLocaleString,
+    }
+
+    // Change API state and application state
+    saveNote(newNote)
+  }
+})
 
 const render = () => {
   contentTarget.innerHTML = `
-        Put some input fields and prompts here
-
-        <button id="saveNote">Save Note</button>
+        <section class="noteForm">
+            <input type="text" id="noteForm--title" placeholder="Enter note title" />
+            <input type="text" id="noteForm--author" placeholder="Your name here" />
+            <textarea id="noteForm--content" placeholder="Note text here"></textarea>
+            <button id="noteForm--saveNote">Save Note</button>
+        </section>
     `
 }
 
