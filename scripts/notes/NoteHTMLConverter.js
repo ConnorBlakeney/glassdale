103d38d9dfa6510335c364a485c54faa71f88410
Connor Blakeney <connorblakeney@yahoo.com>
2020-08-13 14:07:23 -0500

    changed note html

--- scripts/notes/NoteHTMLConverter.js
+++ scripts/notes/NoteHTMLConverter.js
@@ -2,7 +2,7 @@ export const NoteHTMLConverter = (noteObject, criminalObject) => {
   return `
         <section class="note">
             <div class="note--title">Title: ${noteObject.title}</div>
-            <div class="note--criminal">Witness: ${criminalObject.name}</div>
+            <div class="note--criminal">Criminal: ${criminalObject.name}</div>
             <div class="note--content">${noteObject.content}</div>
             <div class="note--author">Author: ${noteObject.author}</div>
             <div class="note--timestamp">Timestamp: ${new Date(
